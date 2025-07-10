86175f57432eb28f9cd743dfc9ecffbd7e71b4fb
한동선 <dongsun_work@handongseon-ui-MacBookAir.local>
2025-07-10 19:07:51 +0900

    MarsKeys 큐브 회전 애니메이션 추가

--- MarsKeys/public/assets/js/barba.js
+++ MarsKeys/public/assets/js/barba.js
@@ -5,21 +5,26 @@ barba.init({
         return gsap.to(data.current.container, {
           rotateY: -90,
           duration: 0.8,
-          transformOrigin: "center left",
+          transformOrigin: "center center",
           ease: "power2.inOut"
         });
       },
       enter(data) {
         gsap.set(data.next.container, {
           rotateY: 90,
-          transformOrigin: "center right"
+          position: 'absolute', // ← 겹치게 하기 위해 필요
+          top: 0,
+          left: 0,
+          width: '100%'
         });
         return gsap.to(data.next.container, {
           rotateY: 0,
           duration: 0.8,
+          transformOrigin: "center center",
           ease: "power2.inOut"
         });
       }
     }]
   });
+  
   
