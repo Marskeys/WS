d5b262cc44c0416d9ee9ea9fdd72b521c1f932ec
한동선 <dongsun_work@handongseon-ui-MacBookAir.local>
2025-07-18 05:13:30 +0900

    BlindLove 사이트맵

--- BlindlyLove/server.js
+++ BlindlyLove/server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static(path.join(__dirname)));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
