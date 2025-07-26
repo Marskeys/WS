efa3bafb80f8e70bc6500300ea8ab6102773a570
한동선 <dongsun_work@handongseon-ui-MacBookAir.local>
2025-07-27 05:19:04 +0900

    BugLoop 수정 중

--- BugLoop/public/assets/js/header.js
+++ BugLoop/public/assets/js/header.js
@@ -29,8 +29,13 @@ document.addEventListener('DOMContentLoaded', () => {
           }
 
           contents.forEach(content => {
-            content.style.display = content.dataset.tab === selectedTab ? 'block' : 'none';
+            content.classList.remove('active-tab');
           });
+          
+          const targetTab = document.querySelector(`.tab-content[data-tab="${selectedTab}"]`);
+          if (targetTab) {
+            targetTab.classList.add('active-tab');
+          }
 
           icons.forEach(i => i.classList.remove('active'));
           icon.classList.add('active');
