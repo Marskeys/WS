0f4f2a0acf3dcbebb3395b8894c886ecdd311ef1
한동선 <dongsun_work@handongseon-ui-MacBookAir.local>
2025-07-26 23:31:21 +0900

    BugLoop 수정

--- BugLoop/public/assets/js/header.js
+++ BugLoop/public/assets/js/header.js
@@ -38,11 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
           icons.forEach(i => i.classList.remove('active'));
           icon.classList.add('active');
 
-          // ✅ 깜빡임 멈춤
-          if (!localStorage.getItem(interactedKey)) {
-            toggleIcon.classList.remove('blink-highlight');
-            localStorage.setItem(interactedKey, 'true');
-          }
+       
         });
       });
 
